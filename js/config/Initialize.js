96f7afd2353ebb6ae45ca299546f0ef1439c2900
ParkJiHo <iks15174@daum.net>
2023-06-17 21:15:35 +0900

    feat: 삼각형의 좌표로 관리하도록 변경

--- js/config/Initialize.js
+++ js/config/Initialize.js
@@ -8,7 +8,7 @@ function initialize(canvas) {
 
 function makeTriange1(canvas) {
     if(triangle1 === null) {
-        triangle1 = new Triangle(canvas, 100, 100, 100, 100)
+        triangle1 = new Triangle(canvas, 100, 100, 100, 100, 90)
     }
     return triangle1
 }
@@ -16,6 +16,10 @@ function makeTriange1(canvas) {
 function drawCanvasBackground(canvas) {
     const ctx = canvas.getContext("2d")
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
 }
 
 function animate(canvas) {
--- js/shape/Triangle.js
+++ js/shape/Triangle.js
@@ -1,52 +1,56 @@
 class Triangle {
-    
-    constructor(canvas, height, width, x, y) {
-        this.canvas = canvas
-        this.height = height
-        this.width = width
-        this.x = x
-        this.y = y
-        new DragAction(canvas, this)
+  constructor(canvas, height, width, x, y, angle) {
+    this.canvas = canvas;
+    this.height = height;
+    this.width = width;
+    this.coordinate = [
+      [x, y],
+      [x, y + height],
+      [x + width, y + height],
+    ];
+    this.angle = angle;
+    new DragAction(canvas, this);
+  }
+
+  getBoard() {
+    if (this.canvas.getContext) {
+      return this.canvas.getContext("2d");
     }
+    return null;
+  }
 
-    getBoard() {
-        if(this.canvas.getContext) {
-            return this.canvas.getContext('2d');
-        }
-        return null
+  draw() {
+    const board = this.getBoard();
+    if (board !== null) {
+      board.beginPath();
+      board.moveTo(this.coordinate[0][0], this.coordinate[0][1]);
+      board.lineTo(this.coordinate[1][0], this.coordinate[1][1]);
+      board.lineTo(this.coordinate[2][0], this.coordinate[2][1]);
+      board.fill();
     }
+  }
 
-    draw() {
-        const board = this.getBoard()
-        if(board !== null) {
-            board.beginPath();
-            board.moveTo(this.x, this.y);
-            board.lineTo(this.x, this.y + this.height);
-            board.lineTo(this.x + this.width, this.y + this.height);
-            board.fill();
-        }
-    }
+  include(x, y) {
+    const a = this.coordinate[0];
+    const b = this.coordinate[1];
+    const c = this.coordinate[2];
+    const p = [x, y];
 
-    include(x, y) {
-        const a = [this.x, this.y]
-        const b = [this.x, this.y + this.height]
-        const c = [this.x + this.width, this.y + this.height]
-        const p = [x, y]
+    const nn = this.calcTriangle(a, b, c);
+    const aa = this.calcTriangle(p, b, c);
+    const bb = this.calcTriangle(a, p, c);
+    const cc = this.calcTriangle(a, b, p);
 
-        const nn = this.calcTriangle(a, b, c)
-        const aa = this.calcTriangle(p, b, c)
-        const bb = this.calcTriangle(a, p, c)
-        const cc = this.calcTriangle(a, b, p)
+    return nn === aa + bb + cc;
+  }
 
-        return (nn === aa + bb + cc)
-    }
+  calcTriangle(a, b, c) {
+    return Math.abs(
+      a[0] * (b[1] - c[1]) + b[0] * (c[1] - a[1]) + c[0] * (a[1] - b[1])
+    );
+  }
 
-    calcTriangle(a, b, c) {
-        return Math.abs((a[0] * (b[1] - c[1])) + (b[0] * (c[1] - a[1])) + (c[0] * (a[1] - b[1])))
-    }
-
-    moveTo(x, y) {
-        this.x += x
-        this.y += y
-    }
+  moveTo(x, y) {
+    this.coordinate = this.coordinate.map(([curX, curY]) => [curX + x, curY + y]);
+  }
 }
